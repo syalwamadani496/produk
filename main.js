b03a0f2c6bef1dd8fd3be5a9ee0051dcc2c07d15
syalwamadani496@gmail.com <syalwamadani496@gmail.com>
2024-02-19 11:24:32 +0700

    komit ke empat menambah daftar produk

---
+++ main.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbLsq27KJU9tD9pHC8GrPUB7LgEPEQbPU",
+  authDomain: "insan-cemerlang-92ee0.firebaseapp.com",
+  projectId: "insan-cemerlang-92ee0",
+  storageBucket: "insan-cemerlang-92ee0.appspot.com",
+  messagingSenderId: "332441427242",
+  appId: "1:332441427242:web:73c31309147ef1dab15253",
+  measurementId: "G-JW04DZL85R"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambilDaftarProduk() {
+  const refDokumen = collection(db, "produk");
+  const kuery = query(refDokumen, orderBy("nama"));
+  const cuplikanKuery = await getDocs(kuery);
+
+  let hasil = [];
+  cuplikanKuery.forEach((doc) => {
+    hasil.push({
+      id: dok.id,
+      nama: dok.data().nama,
+      harga: dok.data().harga,
+      stok: dok.data().stok, 
+    });
+  });
+  
+  return hasil;
+}
