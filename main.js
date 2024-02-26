894f88c05393a5ebb48542f97e193fe075a2e252
syalwamadaniscout@gmail.com <syalwamadaniscout@gmail.com>
2024-02-26 11:00:58 +0700

    komit ke 14 menambahkan tombol simpan

--- main.js
+++ main.js
@@ -46,3 +46,16 @@ export async function ambilDaftarProduk() {
 export function formatAngka(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
+
+export async function tambahProduk(nama, harga, stok) {
+try {
+  const dokRef = await addDoc(collection(db, 'produk'),{
+  nama: nama,
+  harga: harga,
+  stok:stok,
+});
+console.log('Berhasil Menambah Produk'+ dokRef.id);
+} catch(e){
+  console.log('Gagal menambah Produk' + e)
+}
+}
